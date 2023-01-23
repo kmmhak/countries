e8962a33946482c1d71dcecd5210280c2638e6fa
Kristiina Hakkala <kristiina.hakkala@gmail.com>
2023-01-23 19:49:58 +0200

    add BrowserRouter and Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import Home from './components/Home';
+import Router from './components/Router';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Home />
+        <Router />
+      </div>
+    </BrowserRouter>
   );
 }
 
